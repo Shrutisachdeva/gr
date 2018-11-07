a9001c8f7ddb76bbdb8c41cdc483bab73ba86825
Shrutisachdeva <44829401+Shrutisachdeva@users.noreply.github.com>
2018-11-07 12:16:49 +0530

    Add files via upload

---
+++ LoginPage.java
@@ -0,0 +1,36 @@
+package com.fb.automation.pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	
+	@FindBy(id="email")
+	WebElement email;
+	
+	@FindBy(id="pass")
+	WebElement password;
+	
+	@FindBy(id="loginbutton")
+	WebElement loginbutton;
+	
+	public LoginPage(WebDriver driver){
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void typeEmail(String n){
+		email.sendKeys(n);
+	}
+	
+	public void typePass(String p){
+		password.sendKeys(p);
+	}
+	
+	public void login(){
+		loginbutton.click();
+	}
+	
+
+}
